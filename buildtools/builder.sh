3f78465a1b61c8c8074e675a209147a6ef44403a
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2021-07-02 09:43:49 +0200

    Multi arch build

--- buildtools/builder.sh
+++ buildtools/builder.sh
@@ -6,4 +6,5 @@ docker build -f buildtools/Dockerfile.builder -t arlo.fail:builder .
 docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/photo-stream/_site" arlo.fail:builder
 sudo rm -rf $PWD/vendor/bundle/buildtools $PWD/vendor/bundle/README.md $PWD/vendor/bundle/Gemfile* $PWD/vendor/bundle/LICENSE $PWD/vendor/bundle/.git*
 # Build nginx container
-docker build -f buildtools/Dockerfile.nginx -t arlo.fail:nginx .
+docker build -f buildtools/Dockerfile.nginx -t arlo.fail:nginx-amd64 --build-arg ARCH=amd64/ .
+docker build -f buildtools/Dockerfile.nginx -t arlo.fail:nginx-arm64 --build-arg ARCH=arm64/ .
